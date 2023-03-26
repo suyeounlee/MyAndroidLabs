353de6de7d0bf71162974d4b84739fb9a0ff890d
Suyeon Lee <lsy7674@gmail.com>
2023-03-26 19:54:46 -0400

    Finished Week10_ParsingJSON

--- app/src/main/java/algonquin/cst2335/lee00823/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lee00823/MainActivity.java
@@ -2,112 +2,161 @@ package algonquin.cst2335.lee00823;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.lee00823.databinding.ActivityMainBinding;
 
 
 /**
  * @author Su Yeoun Lee
  * @version 1.0
- * This MainActivity class is to practice creating JavaDocs for Lab5.
- * Also, Created checkPasswordComplexity() function to check password complexity for Login.
+ * This MainActivity class is to practice creating JavaDocs for Lab10.
+ * Also, Created
  */
 public class MainActivity extends AppCompatActivity {
 
-    /** This holds the text at the center of the screen */
-    private TextView tv = null;
-    /** This holds the Edit text at the screen */
-    private EditText et = null;
-    /** This holds the button at the screen */
-    private Button btn = null;
+    protected String cityName;
+    protected RequestQueue queue = null;
+    protected ActivityMainBinding binding;
+    Bitmap image;
+    String minTemp;
+    String maxTemp;
+    Runnable Runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
-        et = findViewById(R.id.editText);
-        btn = findViewById(R.id.button);
 
-        btn.setOnClickListener(clk -> {
-            String password = et.getText().toString();
-            checkPasswordComplexity(password);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        });
-    }
+        queue = Volley.newRequestQueue(this);
 
-    /**
-     * This function is to check password complexity before login
-     *
-     * @param pw The String object that we are checking
-     * @return true if the password is complex enough
-     */
-    @SuppressLint("SuspiciousIndentation")
-    boolean checkPasswordComplexity(String pw) {
+        binding.forecastButton.setOnClickListener(click -> {
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL = "";
 
-        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+            try {
 
-        for (int i = 0; i < pw.length(); i++) {
-            char c = pw.charAt(i);
-            if (Character.isDigit(c)) {
-                foundNumber = true;
-            } else if (Character.isUpperCase(c)) {
-                foundUpperCase = true;
-            } else if (Character.isLowerCase(c)) {
-                foundLowerCase = true;
-            } else if(isSpecialCharacter(c)){
-                foundSpecial = true;
+              stringURL = "https://api.openweathermap.org/data/2.5/weather?q="
+                      + URLEncoder.encode(cityName, "UTF-8")
+                      + "&appid=a840d787cf24f31f5762db75e73ebe77&units=metric";
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
-        }
 
-        if (!foundUpperCase) {
-            tv.setText("You shall not pass!");
-            Toast.makeText(getApplicationContext(), "Upper case letter is missing", Toast.LENGTH_SHORT).show();// Say that they are missing an upper case letter;
-            return false;
-        } else if (!foundLowerCase) {
-            tv.setText("You shall not pass!");
-            Toast.makeText(getApplicationContext(), "Lower case letter is missing", Toast.LENGTH_SHORT).show(); // Say that they are missing a lower case letter;
-            return false;
-        } else if (!foundNumber) {
-            tv.setText("You shall not pass!");
-            Toast.makeText(getApplicationContext(), "Number is missing", Toast.LENGTH_SHORT).show(); // Say that they are missing a number;
-            return false;
-        } else if (!foundSpecial) {
-            Toast.makeText(getApplication(), "Special character is missing", Toast.LENGTH_SHORT).show(); // Say that they are missing a character;
-            tv.setText("You shall not pass!");
-            return false;
-        } else
-            tv.setText("Your password meets the requirements.");
-        return true; //only get here if they're all true
 
-    }
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                    (response) -> {
 
-    /**
-     * This function is to check special character contains
-     * @param c The char object that we are checking
-     * @return true if c is one of: #$%^*!@?
-     */
-    boolean isSpecialCharacter(char c) {
-        switch(c) {
-            case '#':
-            case '?':
-            case '*':
-            case '!':
-            case '$':
-            case '%':
-            case '&':
-            case '~':
-            case '@':
-            case '^':
-                return true;
-            default:
-                return false;
-        }
-    }
+                        try {
+                            JSONObject coord = response.getJSONObject("coord");
+                            JSONArray weatherArray = response.getJSONArray("weather");
+                            JSONObject position0 = weatherArray.getJSONObject(0);
+
+                            String description = position0.getString("description");
+                            String iconName = position0.getString("icon");
+
+
+                            JSONObject mainObject = response.getJSONObject("main");
+                            double current = mainObject.getDouble("temp");
+                            double min = mainObject.getDouble("temp_min");
+                            double max = mainObject.getDouble("temp_max");
+                            int humidity = mainObject.getInt("humidity");
+
+                            String imgURL = "https://openweathermap.org/img/w/" + iconName + ".png";
+
+                            String pathname = getFilesDir()+"/" + iconName +".png";
+
+                            File file = new File( pathname );
+
+                            if(file.exists()){
+                                image = BitmapFactory.decodeFile(pathname);
+                            } else {
+                                // let's download that URL and store it as a bitmap. Volley also has an ImageRequest object, like the JSONObjectRequest.
+                                ImageRequest imgReq = new ImageRequest(imgURL, new Response.Listener<Bitmap>() {
+                                    @Override
+                                    public void onResponse(Bitmap bitmap) {
+                                        image = bitmap;
+//second HTTP request to the server and the BitmapFactory.decodeStream() is reading the data from the server.
+// Now you should save the icon to the device so that next time you don't have to download if you've already downloaded it:
+                                        FileOutputStream fOut = null;
+                                        try {
+                                            fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+
+                                            image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                            fOut.flush();
+                                            fOut.close();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+
+                                        }
+                                    } //onResponse
+                                }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+
+                                });
+                                queue.add(imgReq);
+                            } //file exists else
+
+
+                            runOnUiThread( ( ) -> { //To make sure that you are calling these functions on the main GUI thread
+                               //set the text after you've read the strings from the JSON response
+                                binding.temp.setText("The current temperature is " + current);
+                                binding.temp.setVisibility(View.VISIBLE);
+
+                                binding.minTemp.setText("The min temperature is " + min);
+                                binding.minTemp.setVisibility(View.VISIBLE);
+
+                                binding.maxTemp.setText("The max temperature is " + max);
+                                binding.maxTemp.setVisibility(View.VISIBLE);
+
+                                binding.humidity.setText("The humidity is " + humidity);
+                                binding.humidity.setVisibility(View.VISIBLE);
+
+                                binding.description.setText( description);
+                                binding.description.setVisibility(View.VISIBLE);
+
+                                binding.icon.setImageBitmap(image);
+                                binding.icon.setVisibility(View.VISIBLE);
+                            });
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }, //JsonObjectRequest (response)
+
+                    ( error ) -> {
+
+                    }
+
+            ); //JsonObjectRequest
+            queue.add( request );
+        }); //setOnClickListener
+
+    } //onCreate
 }
